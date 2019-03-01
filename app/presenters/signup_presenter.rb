b68e9b67915209e0650d60100de174aa2fccdecc
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-03-01 16:29:01 +0000

    test remove multiple facet

--- app/presenters/signup_presenter.rb
+++ app/presenters/signup_presenter.rb
@@ -29,7 +29,7 @@ class SignupPresenter
   end
 
   def choices?
-    multiple_facet_choice_data.present? || single_facet_choice_data[0]["facet_choices"].present?
+    single_facet_choice_data[0]["facet_choices"].present?
   end
 
   def choices
